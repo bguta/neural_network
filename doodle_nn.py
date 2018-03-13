2da570aa9222ac19bba51a2cb104c1f42a8405c4
bguta <bereket17@hotmail.com>
2018-03-12 23:42:39 -0700

    removed large dataset made a smaller one

--- doodle_nn.py
+++ doodle_nn.py
@@ -1,5 +1,6 @@
-import readData as rd
+# import readData as rd will use later
 import model as md
+import numpy as np
 
 inputSize = 28 * 28  # the pixels space
 outputSize = 3  # the number of choices for objects
@@ -7,9 +8,15 @@ outputSize = 3  # the number of choices for objects
 
 def main():
     """ Make the doodle neural net."""
-    bballTrain1 = rd.read()
-    assert (len(bballTrain1) != 0)
-    assert len(bballTrain1[0]) == inputSize
+    # bballTrain1 = rd.read()
+    Set = np.load("object_nn\\sBasketball.npy")
+    data = []
+
+    for i in range(len(Set)):
+        data.append(list(Set[i]))
+
+    assert (len(data) != 0)
+    assert len(data[0]) == inputSize
     """
     bballTrain2 = rd.read(choice=3)
     assert(len(bballTrain2) != 0 and len(bballTrain2[0]) == inputSize)
@@ -19,30 +26,31 @@ def main():
     assert(len(bballTrain2) != 0 and len(bballTrain2[0]) == inputSize)
     """
 
-    composition = [inputSize, 1000, 1000, 1000,
+    composition = [inputSize, 500, 300, 200, 100,
                    outputSize]  # the network composition
-    md.Network.eta = 0.0001
-    md.Network.alpha = 0.0001
+    md.Network.eta = 0.006
+    md.Network.alpha = 0.15
 
     nn = md.Network(composition)
 
-    goal = [[1, 0, 0]] * len(bballTrain1)  # [[1,0,0]] means basketball
+    goal = [[1, 0, 0]] * len(data)  # [[1,0,0]] means basketball
 
     # train the network
-    train(bballTrain1, goal, nn)
+    train(data, goal, nn)
 
 
 def train(data, goal, net):
     print("Starting to train...")
+    prevE = 0
+    i = 0
     while True:
 
         # input("Enter q to quit else press c: ")
         # start first round of training
         err = 0  # the incured error
+        print("Starting epoch " + str(i))
 
-        for j in range(len(data) // 1000):
-            print("Starting epoch " + str(j))
-
+        for j in range(len(data) // 100):
             net.setInput(data[j])
 
             # feed it through
@@ -53,7 +61,10 @@ def train(data, goal, net):
 
             err += net.getError(goal[j])
 
-            print("\nError: " + str(err))
+        print("\nError: " + str(err))
+        print("Change in error: " + str(err - prevE))
+        prevE = err
+        i += 1
 
         if err <= 0.01:
             print("Done")
