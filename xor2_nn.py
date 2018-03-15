df92e56350f1cf683ba2a218ab2a80e42bf75511
bguta <bereket17@hotmail.com>
2018-03-14 22:00:40 -0700

    made xOR with model2; it is better

---
+++ xor2_nn.py
@@ -0,0 +1,53 @@
+import model2 as m2
+
+
+'''
+This is a test of the model of the neural network using an
+xor truth table dataset
+
+__author__ = Bereket Guta
+'''
+
+
+def main():
+    # the makeup of the network
+    makeUp = [2, 100, 1]
+
+    # activate the network
+    net = m2.Network(makeUp)
+
+    # set the learning rate and momentum to best reach a minimum
+
+    # the input set
+    inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    # the output for each input
+    outputs = [[0], [1], [1], [0]]
+
+    # start
+    print("Training")
+    while True:
+        err = 0
+        for i in range(len(inputs)):
+            net.setInput(inputs[i])
+            net.feedForward()
+            net.backPropagate(outputs[i])
+            err += net.getError(outputs[i])
+        print("error: " + str(err))
+        if err <= 0.01:
+            break
+
+    # testing it out with inputs
+    while True:
+        q = input("Ready:")
+        if q == "q":
+            break
+        a = float(input("type 1st input: "))
+        b = float(input("type 2nd input: "))
+
+        net.setInput([a, b])
+        net.feedForward()
+        print(str(net.getResults()))
+
+
+if __name__ == '__main__':
+    main()
