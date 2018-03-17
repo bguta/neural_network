7a2c67366566749be6f01c4c87812d27b1f3141d
bguta <bereket17@hotmail.com>
2018-03-16 21:48:04 -0700

    fixed and made a way to format paint images to the dataset specifications

---
+++ data/formatImage.py
@@ -0,0 +1,33 @@
+import numpy as np
+from PIL import Image as Im
+import PIL.ImageOps as imo
+import matplotlib.image as image
+from scipy.misc import imsave
+
+""" save a 28 by 28 doodle made in paint as a png in data/ then
+    run this on the image to format it. This overrides the image
+    so make sure to have a backup
+"""
+
+
+def main():
+    imName = input("Please enter the name of the image (ex: sun.png): ")
+
+    im = Im.open(imName)
+
+    im = im.convert('L')
+    im = imo.invert(im)
+    im = im.convert('RGB')
+    im.save(imName)
+
+    pic = image.imread(imName)
+    gpic = rgb2gray(pic)
+    imsave(imName, gpic)
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+
+if __name__ == "__main__":
+    main()
